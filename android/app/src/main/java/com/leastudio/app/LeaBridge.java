5410ba1b20fb826ad53cef785ea13f5011cb70a9
davidc2115 <davidc2115@users.noreply.github.com>
2026-09-26 14:58:34 +0000

    Fix compile: import FileInputStream manquant dans LeaBridge

--- android/app/src/main/java/com/leastudio/app/LeaBridge.java
+++ android/app/src/main/java/com/leastudio/app/LeaBridge.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
